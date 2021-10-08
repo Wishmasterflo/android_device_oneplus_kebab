45235aab76081397fd9582f36ae9b9c685aaf874
Wishmasterflo <72379104+Wishmasterflo@users.noreply.github.com>
2021-10-08 19:17:34 +0200

    Adjusted for Kebab
    Adjusted for Kebab

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,79 +1,70 @@
-FDEVICE="kebab"
+export TARGET_ARCH="arm64-v8a"
 
-fox_get_target_device() {
-local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
-   if [ -n "$chkdev" ]; then 
-      FOX_BUILD_DEVICE="$FDEVICE"
-   else
-      chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
-      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
-   fi
-}
+# Some about us
+export FOX_VERSION="R11.1"
+export OF_MAINTAINER=Wishmasterflo
 
-if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
-   fox_get_target_device
-fi
+# Build environment stuffs
+export FOX_BUILD_DEVICE="kebab"
+export ALLOW_MISSING_DEPENDENCIES=true
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
+export OF_DISABLE_MIUI_SPECIFIC_FEATURES="0"
+export LC_ALL="C"
+export OF_TARGET_DEVICES="kebab, Kebab, OnePlus8t, OnePlus 8t"
+export TARGET_DEVICE_ALT="kebab, Kebab, OnePlus8t, OnePlus 8t"
 
-if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-   	export TW_DEFAULT_LANGUAGE="en"
-	export LC_ALL="C"
- 	export ALLOW_MISSING_DEPENDENCIES=true
-	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
-	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
-	export OF_USE_GREEN_LED=0
-	
-	# Kebab Specific
-	export OF_AB_DEVICE=1
-	export OF_FBE_METADATA_MOUNT_IGNORE=1
-	export OF_STATUS_INDENT_LEFT=254
-	export OF_TARGET_DEVICES="kebab,Kebab,OnePlus8t,Oneplus 8t"
-	export TARGET_DEVICE_ALT="kebab,Kebab,OnePlus8t,Oneplus 8t"
-	export OF_USE_MAGISKBOOT=1
-	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-	export OF_NO_MIUI_PATCH_WARNING=1
-	export FOX_USE_BASH_SHELL=1
-	export FOX_ASH_IS_BASH=1
-	export FOX_USE_TAR_BINARY=1
-	export FOX_USE_SED_BINARY=1
-	export FOX_USE_XZ_UTILS=1
-	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
-  export OF_QUICK_BACKUP_LIST="/boot;/data;"
-	export OF_PATCH_AVB20=1
-  export FOX_DELETE_AROMAFM=1
-  export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"
-  export FOX_ENABLE_APP_MANAGER=1
+# Use magisk boot for patching
+export OF_USE_MAGISKBOOT=1
+export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
 
-    # OTA
-    export OF_KEEP_DM_VERITY=1
-    export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
-    export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
-    export OF_DISABLE_MIUI_OTA_BY_DEFAULT=0
-    
-    # R11.1 Settings
-    export FOX_VERSION="R11.1"
-    export OF_MAINTAINER="Wishmasterflo"
-    #export OF_MAINTAINER_AVATAR="misc/Sushrut1101.png"
-    export FOX_BUILD_TYPE="Unofficial"
+# We have a/b partitions
+export OF_AB_DEVICE="1"
 
-	# Screen Settings
-	export OF_SCREEN_H=2400
-	export OF_STATUS_H=120
-	#export OF_STATUS_INDENT_LEFT=48 # For Most of the Devices
-	#export OF_STATUS_INDENT_LEFT=245 # Avicii Specific
-	export OF_STATUS_INDENT_RIGHT=48
-	export OF_ALLOW_DISABLE_NAVBAR=0
+# Screen specifications
+#export OF_SCREEN_H=2340
+export OF_STATUS_H=-80
+export OF_STATUS_INDENT_LEFT=48
+export OF_STATUS_INDENT_RIGHT=48
+export OF_ALLOW_DISABLE_NAVBAR=0
 
-	# Run a process after formatting data to work-around MTP issues
-	export OF_RUN_POST_FORMAT_PROCESS=1
-	
-	# let's see what are our build VARs
-	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
-  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
-  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
-   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
-  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
- 	fi
-fi
+# Device stuff
+export OF_KEEP_FORCED_ENCRYPTION="1"
+export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
+export OF_FBE_METADATA_MOUNT_IGNORE=1
+export OF_PATCH_AVB20=1
+
+# Run a process after formatting data to work-around MTP issues
+export OF_RUN_POST_FORMAT_PROCESS=1
+
+# OTA for custom ROMs
+export OF_SUPPORT_ALL_PAYLOAD_OTA_UPDATES=1
+export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+
+# OTA
+export OF_KEEP_DM_VERITY=1
+export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+export OF_DISABLE_MIUI_OTA_BY_DEFAULT=0
+
+# For some reason this is dumb and necessary
+export FOX_BUGGED_AOSP_ARB_WORKAROUND="1601559499"
+
+# -- add settings for R11 --
+export FOX_R11=1
+export FOX_ADVANCED_SECURITY=1
+export FOX_ENABLE_APP_MANAGER=1
+export OF_USE_TWRP_SAR_DETECT=1
+export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+export OF_QUICK_BACKUP_LIST="/boot;/data;/system_image;/vendor_image;"
+# -- end R11 settings --
+
+# Add some extras
+export FOX_USE_ZIP_BINARY="1"
+export FOX_USE_TAR_BINARY="1"
+export FOX_ASH_IS_BASH="1"
+export FOX_REPLACE_BUSYBOX_PS="1"
+export FOX_USE_BASH_SHELL="1"
+export FOX_USE_LZMA_COMPRESSION="1"
+export FOX_USE_NANO_EDITOR="1"
