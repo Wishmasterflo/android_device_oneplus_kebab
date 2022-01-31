ceda046365fc4f505d6e6d3ad7f370d345c68996
Wishmasterflo <72379104+Wishmasterflo@users.noreply.github.com>
2022-01-31 10:04:45 +0100

    Adjusted screen settings
    Adjusted screen settings

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -22,8 +22,8 @@ export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
 export OF_AB_DEVICE="1"
 
 # Screen specifications
-export OF_SCREEN_H=1920
-export OF_SCREEN_W=1080
+#export OF_SCREEN_H=1920
+#export OF_SCREEN_W=1080
 export OF_STATUS_H=100
 export OF_STATUS_INDENT_LEFT=48
 export OF_STATUS_INDENT_RIGHT=48
