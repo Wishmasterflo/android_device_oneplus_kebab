eb1523f7dbd0c86449c3a147b0d11a3c2d36cd78
Wishmasterflo <72379104+Wishmasterflo@users.noreply.github.com>
2022-07-15 00:52:06 +0200

    adjusted call command
    adjusted call command

--- Android.mk
+++ Android.mk
@@ -27,7 +27,7 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),kebab)
 
-include $(call all-makefiles-under,$(LOCAL_PATH))
+include $(call all-subdir-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 
